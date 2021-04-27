0f7f6b02033392c8461b8726bea6883f2783d2c6
Darkzarich <darkzarichv2@gmail.com>
2021-04-27 10:39:12 +0300

    change baseURL to my-json-server

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3004`,
+  baseURL: `https://my-json-server.typicode.com/Darkzarich/my-nuxt-app`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
