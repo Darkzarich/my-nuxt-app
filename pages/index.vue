1bf1e67baed0604549efe9cbb33a9851a9ccea88
Darkzarich <darkzarichv2@gmail.com>
2021-04-27 08:32:22 +0300

    add request all events server side & EventCard component & handling errors

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard.vue'
 export default {
+  components: { EventCard },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((res) => {
+        return {
+          events: res.data,
+        }
+      })
+      .catch(() => {
+        error({ statusCode: 503, message: 'Unable to fetch events' })
+      })
+  },
   head() {
     return {
       title: 'Event Listing',
